175db4e7d9f0017601ba307363372065a41b5bad
mdi <mihaidicianu@gmail.com>
2022-02-03 18:57:12 +0200

    Updates

---
+++ precache-manifest.3293887cad1793c3dad80e79518c8136.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15b0729d637bf0466ccb6348392aadcb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6179fa5ccceed9d4777",
+    "url": "/static/js/2.fa5a842b.chunk.js"
+  },
+  {
+    "revision": "a0a3bc599e5949121a94",
+    "url": "/static/js/main.45c3e1ac.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
