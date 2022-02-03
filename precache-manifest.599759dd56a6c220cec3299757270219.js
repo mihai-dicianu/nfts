cbf2eca1805b9a322f2afa8e698a46ca9f924b38
mdi <mihaidicianu@gmail.com>
2022-02-03 19:00:37 +0200

    Updates

---
+++ precache-manifest.599759dd56a6c220cec3299757270219.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f7a12b8462d537984fa24e5b299fd79",
+    "url": "/nfts/index.html"
+  },
+  {
+    "revision": "41b037952a8defa05307",
+    "url": "/nfts/static/js/2.d0f5aff7.chunk.js"
+  },
+  {
+    "revision": "a0a3bc599e5949121a94",
+    "url": "/nfts/static/js/main.45c3e1ac.chunk.js"
+  },
+  {
+    "revision": "42c204e5386997c03c4c",
+    "url": "/nfts/static/js/runtime-main.da73423d.js"
+  }
+]);
