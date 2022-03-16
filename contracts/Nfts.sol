365d46a95e63febda54d019d34ba5b801aa0d7f2
mdi <mihaidicianu@gmail.com>
2022-03-16 18:06:27 +0200

    Add nft SC and gitignore file

---
+++ contracts/Nfts.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract Nfts is ERC721URIStorage {
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("NFTs Contract", "NFTS") {
+        function totalSupply() public view returns (uint256) {
+            return _tokenIds.current();
+        }
+    }
+
+    function contractURI() public pure returns (string memory) {
+        return "JSON link to contract";
+    }
+
+    function mintItem(address minter, string memory tokenURI) public onlyOwner returns (uint256) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(minter, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        return newItemId;
+    }
+}
+
+
+
