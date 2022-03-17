a3e28d258cd489189ede321b8c08559a721faf83
mdi <mihaidicianu@gmail.com>
2022-03-17 13:03:02 +0200

    Update project config

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,7 @@
+require('dotenv').config();
+const { API_URL, API_URL_RINKEBY, PRIVATE_KEY, PRIVATE_KEY_RINKEBY } = process.env;
 const path = require("path");
+var HDWalletProvider = require("truffle-hdwallet-provider");
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -7,7 +10,24 @@ module.exports = {
   networks: {
     develop: {
       port: 8545
-    }
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(PRIVATE_KEY, API_URL)
+      },
+      network_id: 3,
+      gas: 3000000      //make sure this gas allocation isn't over 4M, which is the max
+    },
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(
+          PRIVATE_KEY_RINKEBY,
+          API_URL_RINKEBY
+        ),
+      network_id: 4, // Rinkeby's id
+      gas: 4500000,
+      gasPrice: 10000000000,
+    },
   },
   compilers: {
     solc: {
